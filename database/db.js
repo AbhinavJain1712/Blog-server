bcbb15e6a2247b4d9e17e7e9bb5b498063aeef83
AbhinavJain1712 <106861044+AbhinavJain1712@users.noreply.github.com>
2024-07-03 08:56:40 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.DB_URL);
 const Connection = async (URL) => {
   try {
-    await mongoose.connect(URL, { useNewUrlParser: true });
+    await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Database connected successfully");
   } catch (error) {
-    console.log("Error while connecting to the database ", error);
+    console.log("Error while connecting to the database", error);
   }
 };
+
+// const DB_URL = process.env.DB_URL;
+// Connection(DB_URL);
 
 export default Connection;
